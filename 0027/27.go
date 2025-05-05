82324794b040646d6c17281ff17762f9c780fe38
luhongcheng <2437383481@qq.com>
2025-05-05 14:02:52 +0800

    Removes IDE-specific configuration files and improves algorithm
    Deletes .idea directory to clean up project-specific IDE settings
    Optimizes removeElement function with an in-place solution
    Updates Go version in go.mod to align with project requirements

--- 0027/27.go
+++ 0027/27.go
@@ -2,6 +2,12 @@ package main
 
 func removeElement(nums []int, val int) int {
     n:=len(nums)
-    
-	
+    i:=0
+    for j:=0;j<n;j++{
+        if nums[j]!=val{
+            nums[i]=nums[j]
+            i++
+        }
+    }
+    return i
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module leetcode-go
 
-go 1.24
+go 1.23
